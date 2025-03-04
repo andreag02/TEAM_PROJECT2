403fe9a2c9cd7e485e2e24541bd7ac8ba3d53058
Andrea G <andrea.garza20@utrgv.edu>
2025-03-04 14:07:45 -0600

    Added more tests

--- test/cypress/e2e/navigation.cy.ts
+++ test/cypress/e2e/navigation.cy.ts
@@ -26,4 +26,28 @@ describe("Navigation Test", () => {
     cy.url().should("include", "/login");
     cy.contains("Sign in").should("be.visible");
   });
+
+  it("should navigate to the signup page when clicking the link on the sign in page", () => {
+    cy.visit("http://localhost:3000/login");
+    cy.contains("Sign Up").click();
+    cy.url().should("include", "/signup");
+    cy.contains("Sign up").should("be.visible");
+
+    cy.visit("http://localhost:3000/login");
+    cy.contains("Don't have an account? Sign up").click();
+    cy.url().should("include", "/signup");
+    cy.contains("Sign up").should("be.visible");
+  });
+
+  it("should navigate to the sign in page when clicking the link on the signup page", () => {
+    cy.visit("http://localhost:3000/signup");
+    cy.contains("Log In").click();
+    cy.url().should("include", "/login");
+    cy.contains("Sign in").should("be.visible");
+
+    cy.visit("http://localhost:3000/signup");
+    cy.contains("Already have an account? Sign in").click();
+    cy.url().should("include", "/login");
+    cy.contains("Sign in").should("be.visible");
+  });
 });
